957b3e63e6a43c6875250c488337ec98e8603f66
Regan Losey <regan.losey@gmail.com>
2022-01-21 15:31:39 -0700

    Add price property to objects

--- server.js
+++ server.js
@@ -55,9 +55,9 @@ app.post('/api/v1/brews', (req, res) => {
     `})
     }
   }
-  const { productName, type} = brew;
-  app.locals.brews.push({ productName, type, id})
-  res.status(201).json({productName, type, id})
+  const { productName, type } = brew;
+  app.locals.brews.push({ productName, type, id })
+  res.status(201).json({ productName, type, id })
   console.log(req, res)
 })
 
@@ -67,186 +67,217 @@ app.locals.brews =
       id: "1",
       productName: "Amaretto",
       type: "Coffee",
+      price: 10,
       hasCaffeine: true
     },
     {
       id: "2",
       productName: "Columbian",
       type: "Coffee",
+      price: 10,
       hasCaffeine: true
     },
     {
       id: "3",
-      productName: "Decaf Irish Cream",
+      productName: "Peruvian",
       type: "Coffee",
+      price: 10,
       hasCaffeine: false
     },
     {
       id: "4",
-      productName: "Caffe Latte",
+      productName: "",
       type: "Espresso",
+      price: 10,
       hasCaffeine: true
     },
     {
       id: "5",
       productName: "Caffe Mocha",
       type: "Espresso",
+      price: 10,
       hasCaffeine: true
     },
     {
       id: "6",
       productName: "Decaf Espresso",
       type: "Espresso",
+      price: 10,
       hasCaffeine: false
     },
     {
       id: "7",
       productName: "Regular Espresso",
       type: "Espresso",
+      price: 10,
       hasCaffeine: true
     },
     {
       id: "8",
       productName: "Chamomile",
       type: "Herbal Tea",
+      price: 10,
       hasCaffeine: false
     },
     {
       id: "9",
       productName: "Lemon",
       type: "Herbal Tea",
+      price: 10,
       hasCaffeine: false
     },
     {
       id: "10",
       productName: "Mint",
       type: "Herbal Tea",
+      price: 10,
       hasCaffeine: false
     },
     {
       id: "11",
       productName: "Darjeeling",
       type: "Tea",
+      price: 10,
       hasCaffeine: true
     },
     {
       id: "12",
       productName: "Earl Grey",
       type: "Tea",
+      price: 10,
       hasCaffeine: true
     },
     {
       id: "13",
       productName: "Chai Tea",
       type: "Tea",
+      price: 10,
       hasCaffeine: true
     },
     {
       id: "14",
       productName: "Lavender",
       type: "Tea",
+      price: 10,
       hasCaffeine: false
     },
     {
       id: "15",
       productName: "Peppermint Tea",
       type: "Tea",
+      price: 10,
       hasCaffeine: false
     },
     {
       id: "16",
       productName: "Valerian Root Tea",
       type: "Tea",
+      price: 10,
       hasCaffeine: false
     },
     {
       id: "17",
       productName: "Cuban",
       type: "Coffee",
+      price: 10,
       hasCaffeine: true
     },
     {
       id: "18",
       productName: "Peruvian ",
       type: "Coffee",
+      price: 10,
       hasCaffeine: true
     },
     {
       id: "19",
       productName: "Americano",
       type: "Espresso",
+      price: 10,
       hasCaffeine: true
     },
     {
       id: "20",
       productName: "English Breakfast",
       type: "Tea",
+      price: 10,
       hasCaffeine: false
     },
     {
       id: "21",
       productName: "Jasmine",
       type: "Tea",
+      price: 10,
       hasCaffeine: false
     },
     {
       id: "22",
       productName: "Oolong",
       type: "Tea",
+      price: 10,
       hasCaffeine: false
     },
     {
       id: "23",
       productName: "Passionflower",
       type: "Tea",
+      price: 10,
       hasCaffeine: false
     },
     {
       id: 24,
       productName: "Ginger",
       type: "Tea",
+      price: 10,
       hasCaffeine: false
     },
     {
       id: "25",
       productName: "Yerba Mate",
       type: "Tea",
+      price: 10,
       hasCaffeine: true
     },
     {
       id: "26",
       productName: "White Tea",
       type: "Tea",
+      price: 10,
       hasCaffeine: false
     },
     {
       id: "27",
       productName: "Hibiscus",
       type: "Tea",
+      price: 10,
       hasCaffeine: false
     },
     {
       id: "28",
       productName: "Blueberry",
       type: "Tea",
+      price: 10,
       hasCaffeine: false
     },
     {
       id: "29",
       productName: "Vanilla",
       type: "Tea",
+      price: 10,
       hasCaffeine: false
     },
     {
       id: "30",
       productName: "Spiced Apple",
       type: "Tea",
+      price: 10,
       hasCaffeine: false
     },
     {
       id: "31",
       productName: "Blood Orange",
       type: "Tea",
+      price: 10,
       hasCaffeine: false
     }
   ]

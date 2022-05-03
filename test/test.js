c94e0e6e3edebcb7a48677a2b09fee0986194f88
Regan Losey <regan.losey@gmail.com>
2022-05-03 10:56:07 -0700

    Refactor test with new endpoints and test for invalid POST

--- test/test.js
+++ test/test.js
@@ -8,7 +8,7 @@ chai.use(chaiHttp);
 describe('Coffee Server', () => {
   it('should be able to make a successful GET request', (done) => {
     chai.request(server)
-      .get('/api/v1/brews')
+      .get('/brews')
       .end((err, res) => {
         res.should.have.status(200);
         done()
@@ -18,7 +18,7 @@ describe('Coffee Server', () => {
   it('should return a single brew when given an id', (done) => {
     const brewId = 5;
     chai.request(server)
-      .get('/api/v1/brews/' + brewId)
+      .get('/brews/' + brewId)
       .end((err, res) => {
         res.should.have.status(200);
         res.body.should.be.a('object');
@@ -30,10 +30,10 @@ describe('Coffee Server', () => {
       })
   })
 
-  it('should be able to POST a new brew to the list', (done) => {
+  it('should NOT be able to POST a new brew without the correct API key present', (done) => {
     const brewId = Date.now();
     chai.request(server)
-      .post('/api/v1/brews')
+      .post('/brews')
       .send({
         'id': brewId,
         'productName': 'Chai',
@@ -42,24 +42,26 @@ describe('Coffee Server', () => {
         'hasCaffeine': true
       })
       .end((err, res) => {
-        res.should.have.status(201);
+        res.should.have.status(401);
         res.should.be.json;
         res.body.should.be.a('object');
-        res.body.should.have.property('productName').eq('Chai')
-        res.body.should.have.property('type').eq('Tea');
-        res.body.should.have.property('price').eq(10);
-        res.body.should.have.property('hasCaffeine').eq(true);
+        res.body.should.have.property('error').eq("Howdy🤠 I'm currently disallowing POST requests. If you'd like access, please email me to discuss your usage :) ")
       })
     done()
   })
   it('should be able to fetch and return data from external API', (done) => {
     chai.request(server)
-    .get('/api/v1/newBrews')
+    .get('/brews')
     .end((err, res) => {
       res.should.have.status(200);
       res.should.be.a('object');
     })
     done()
   })
+
+  // it('should be able to POST a new object if all parameters are present', (done) => {
+  //   chai.request(server)
+  //   .post(/brews)
+  // })
 
 })
